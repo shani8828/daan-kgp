e53b814cfe23c815738e75e89a3231644a657dca
shani8828 <shani.maurya.iitkgp@gmail.com>
2025-06-19 00:01:53 +0530

    Banner.jsx updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -51,7 +51,7 @@ const Banner = () => {
                       href="https://maps.app.goo.gl/bJGDh9x82obhCeyR9"
                       target="_blank"
                       title="Map"
-                      className="flex items-center gap-4 hover:translate-x-2 transition-all duration-300"
+                      className="flex items-center gap-4 hover:translate-x-2 transition-all duration-300 hover:text-red-500"
                     >
                       <FaHelicopter className="text-4xl h-4 w-4 md:h-12 md:w-12 shadow-lg shadow-red-200 p-2 md:p-4 rounded-full bg-red-100 border border-red-300 dark:bg-red-400" />
                       <p>Helipad</p>
@@ -60,7 +60,7 @@ const Banner = () => {
                       href="https://maps.app.goo.gl/7hXBb4WKcHD6HqR87"
                       target="_blank"
                       title="Map"
-                      className="flex items-center gap-4 hover:translate-x-2 transition-all duration-300"
+                      className="flex items-center gap-4 hover:translate-x-2 transition-all duration-300 hover:text-red-500"
                     >
                       <FaBus className="text-4xl h-4 w-4 md:h-12 md:w-12 shadow-lg shadow-green-200 p-2 md:p-4 rounded-full bg-green-100 border border-green-300 dark:bg-green-400" />
                       <p>Hanger</p>
@@ -71,7 +71,7 @@ const Banner = () => {
                       href="https://maps.app.goo.gl/1gqV1VTQ4cLr6Ut69"
                       target="_blank"
                       title="Map"
-                      className="flex items-center gap-4 hover:translate-x-2 transition-all duration-300"
+                      className="flex items-center gap-4 hover:translate-x-2 transition-all duration-300 hover:text-red-500"
                     >
                       <GiTrophy className="text-4xl h-4 w-4 md:h-12 md:w-12 shadow-lg shadow-blue-200 p-2 md:p-4 rounded-full bg-blue-100 border border-blue-300 dark:bg-blue-400" />
                       <p>Gymkhana</p>
@@ -80,7 +80,7 @@ const Banner = () => {
                       href="https://maps.app.goo.gl/meC9B6u3ZPxQ8sPA8"
                       target="_blank"
                       title="Map"
-                      className="flex items-center gap-4 hover:translate-x-2 transition-all duration-300"
+                      className="flex items-center gap-4 hover:translate-x-2 transition-all duration-300 hover:text-red-500"
                     >
                       <FaBuilding className="text-4xl h-4 w-4 md:h-12 md:w-12 shadow-lg shadow-yellow-200 p-2 md:p-4 rounded-full bg-yellow-100 border border-yellow-300 dark:bg-yellow-400" />
                       <p>Nehru Museum</p>
