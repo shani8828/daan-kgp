d6a15d0cc2e1d5f2ab763fcad280ad8ecc0ca1ae
shani8828 <shani.maurya.iitkgp@gmail.com>
2025-07-02 21:01:24 +0530

    Internpedia added

--- src/components/FresherPlace/CDCInternData.js
+++ src/components/FresherPlace/CDCInternData.js
@@ -5,5 +5,23 @@ const CDCInternData = [
     heading: "CV Making Guide",
     href: "https://docs.google.com/document/d/13mIutFXSX-5i53dC5Lo282pKpzxWM6WGY8kstUdwYqY/edit?usp=sharing",
   },
+  {
+    catching: "Ready to land your dream internship?",
+    cover: "Internpedia offers insights and learnings from last year's journey.",
+    heading: "Internpedia 2025",
+    href: "https://drive.google.com/file/d/1yOqF891CqhK-oBnvfUrfTsxkwvBj8Ly-/view?usp=sharing",
+  },
+  {
+    catching: "Ready to land your dream internship?",
+    cover: "Internpedia offers insights and learnings from previous years journey.",
+    heading: "Internpedia 2024",
+    href: "https://drive.google.com/file/d/1v8z-hbtTCzEQSxNdYhFSpHkJKnIXvKoP/view?usp=sharing",
+  },
+  {
+    catching: "Ready to land your dream internship?",
+    cover: "Internpedia offers insights and learnings from previous years journey.",
+    heading: "Internpedia 2023",
+    href: "https://drive.google.com/file/d/15cnaUUXuST-MxuIvpi3MzsTUjNWlpDE8/view?usp=sharing",
+  },
 ];
 export default CDCInternData;
