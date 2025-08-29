6b6d581ff648a3ad87aacd6fcba3f350261e238f
shani8828 <shani.maurya.iitkgp@gmail.com>
2025-08-29 12:07:44 +0530

    Navbar dark theme done

---
+++ src/components/Navbar/JSFiles/NavbarData.js
@@ -0,0 +1,12 @@
+export const NavbarLinks = [
+  { name: "Home", link: "/" },
+  { name: "Our Fam", link: "/our-fam" },
+  { name: "Our Bright Minds", link: "/our-bright-minds" },
+];
+
+export const dropdown = [
+  { name: "ERP Place", link: "/erp-place" },
+  { name: "Fresher Place", link: "/fresher-place" },
+  { name: "Academic Place", link: "/academic-place" },
+  { name: "CDC Intern Place", link: "/cdc-intern-place" },
+];
