03d7ebd9990d6ba6618733f89d7a79681a8acc8d
shani8828 <shani.maurya.iitkgp@gmail.com>
2025-08-30 02:41:29 +0530

    FresherPlace type dark theme done!

--- src/pages/AcademicPlace.jsx
+++ src/pages/AcademicPlace.jsx
@@ -14,9 +14,9 @@ const AcademicPlace = () => {
           content="This is the place where every fresher gets the requirements. This page facilitates them to get a smooth transition to college life."
         />
       </Helmet>
-      <div className="dark:bg-gray-900 dark:text-white bg-gray-50 py-14 pt-20">
+      <div className="dark:bg-gray-900 dark:text-gray-400 bg-gray-100 py-14 pt-20">
         <section data-aos="fade-up" className="container ">
-          <h1 className="my-8 border-l-8 border-red-300 py-2 pl-2 text-3xl font-bold w-full ">
+          <h1 className="my-8 border-l-8 border-red-300 dark:border-gray-400 py-2 pl-2 text-3xl font-bold w-full ">
             Academic Place
           </h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8">
