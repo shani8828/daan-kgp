18295005ee21555a4e78f69a14a2378e9b918c3b
shani8828 <shani.maurya.iitkgp@gmail.com>
2025-08-29 16:21:35 +0530

    CR dark theme updated

---
+++ src/components/CR/CRData.js
@@ -0,0 +1,29 @@
+ export const CRData = [
+  {
+    id: 0,
+    name: "Mr. Shani Maurya",
+    status: "Current",
+    hall: "MMM",
+    img: "https://res.cloudinary.com/dcwwptwzt/image/upload/v1748023103/Shani_Maurya_cpux6d.avif",
+    session: "2024-25",
+    mobile: "+919555439091",
+  },
+  {
+    id: 1,
+    name: "Mr. Ashish Meena",
+    status: "Ex",
+    hall: "RP",
+    img: "https://res.cloudinary.com/dcwwptwzt/image/upload/v1747740285/Ashish_kumar_Meena_z9nbod.avif",
+    session: "2023-24",
+    mobile: "+917340050926",
+  },
+  {
+    id: 2,
+    name: "Mr. Abhinaw Anand",
+    status: "Ex",
+    hall: "Nehru",
+    img: "https://res.cloudinary.com/dcwwptwzt/image/upload/v1747723143/Avatar_avs1qx.avif",
+    session: "2022-23",
+    mobile: "+919653408028",
+  },
+];
